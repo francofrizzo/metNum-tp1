ac5cc1699b94c4d1c5f038690fb0252906afa3bb
Franco Frizzo <francofrizzo@gmail.com>
2015-09-03 17:59:34 -0300

    Agregados gráficos para los casos nuevos que hizo Lu

--- src/exp/2-isoSegunGranularidad-const/graficos.m
+++ src/exp/2-isoSegunGranularidad-const/graficos.m
@@ -1,10 +1,16 @@
 addpath ../../tools
 
 horno('2-real.in', '2-real.sol', '2-real-iso2.sol', 'graficos/2-real', 'graficos/2-real-iso');
+horno('casoA-angulos/2a-3.in', 'casoA-angulos/2a-3.sol', 'casoA-angulos/2a-3-iso2.sol', 'graficos/2a-3', 'graficos/2a-3-iso');
+horno('casoA-angulos/2a-5.in', 'casoA-angulos/2a-5.sol', 'casoA-angulos/2a-5-iso2.sol', 'graficos/2a-5', 'graficos/2a-5-iso');
+horno('casoA-angulos/2a-8.in', 'casoA-angulos/2a-8.sol', 'casoA-angulos/2a-8-iso2.sol', 'graficos/2a-8', 'graficos/2a-8-iso');
 horno('casoA-angulos/2a-10.in', 'casoA-angulos/2a-10.sol', 'casoA-angulos/2a-10-iso2.sol', 'graficos/2a-10', 'graficos/2a-10-iso');
 horno('casoA-angulos/2a-30.in', 'casoA-angulos/2a-30.sol', 'casoA-angulos/2a-30-iso2.sol', 'graficos/2a-30', 'graficos/2a-30-iso');
 horno('casoA-angulos/2a-50.in', 'casoA-angulos/2a-50.sol', 'casoA-angulos/2a-50-iso2.sol', 'graficos/2a-50', 'graficos/2a-50-iso');
 horno('casoA-angulos/2a-70.in', 'casoA-angulos/2a-70.sol', 'casoA-angulos/2a-70-iso2.sol', 'graficos/2a-70', 'graficos/2a-70-iso');
+horno('casoB-radios/2b-3.in', 'casoB-radios/2b-3.sol', 'casoB-radios/2b-3-iso2.sol', 'graficos/2b-3', 'graficos/2b-3-iso');
+horno('casoB-radios/2b-5.in', 'casoB-radios/2b-5.sol', 'casoB-radios/2b-5-iso2.sol', 'graficos/2b-5', 'graficos/2b-5-iso');
+horno('casoB-radios/2b-8.in', 'casoB-radios/2b-8.sol', 'casoB-radios/2b-8-iso2.sol', 'graficos/2b-8', 'graficos/2b-8-iso');
 horno('casoB-radios/2b-10.in', 'casoB-radios/2b-10.sol', 'casoB-radios/2b-10-iso2.sol', 'graficos/2b-10', 'graficos/2b-10-iso');
 horno('casoB-radios/2b-30.in', 'casoB-radios/2b-30.sol', 'casoB-radios/2b-30-iso2.sol', 'graficos/2b-30', 'graficos/2b-30-iso');
 horno('casoB-radios/2b-50.in', 'casoB-radios/2b-50.sol', 'casoB-radios/2b-50-iso2.sol', 'graficos/2b-50', 'graficos/2b-50-iso');
